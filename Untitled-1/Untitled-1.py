e407258fe3d4a35908520c530d326172da441be0
polonecmichal <polonecmichal36@gmail.com>
2026-03-26 19:58:00 +0100

    test

--- Untitled-1/Untitled-1.py
+++ Untitled-1/Untitled-1.py
@@ -1,16 +1,11 @@
-vstup = input("zadaj mi co idem heslovat:")
-kluc ="abc"
-novy_kluc = kluc * (len(vstup)//len(kluc)) + kluc[:len(vstup)%len(kluc):]
-print(len(novy_kluc), len(vstup))
-vystup = ""
 
-for i in range(len(vstup)):
-    znak = ord(vstup[i])
-    if ord('a') <= ord(vstup[i]) and ord(vstup[i]) <= ord('z') :
-        posun = ord(novy_kluc[i])-ord('a')+1
-        ord(vstup[i]) + posun % 26
-        vystup += chr((ord(vstup[i]) - ord('a') + posun)%26 + ord('a'))
-    else:
-        vystup += vstup[i]
+import py5
 
-print(vystup)
+def setup():
+    py5.size(200, 200)
+    py5.rect_mode(py5.CENTER)
+
+def draw():
+    py5.rect(py5.mouse_x, py5.mouse_y, 10, 10)
+
+py5.run_sketch()
